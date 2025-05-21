d5160b9dd55bfd82588e933417f3465be4376645
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-05-21 17:09:34 +0430

    all style and feature cars component by Muhammad Hakeem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <!-- Favicon -->
+<link rel="shortcut icon" href="images/favicon.ico" />
+
+<!-- bootstrap -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+
+<!-- flaticon -->
+<link rel="stylesheet" type="text/css" href="css/flaticon.css" />
+
+<!-- mega menu -->
+<link rel="stylesheet" type="text/css" href="css/mega-menu/mega_menu.css" />
+
+<!-- font awesome -->
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+
+<!-- owl-carousel -->
+<link rel="stylesheet" type="text/css" href="css/owl-carousel/owl.carousel.css" />
+
+<!-- magnific-popup -->
+<link rel="stylesheet" type="text/css" href="css/magnific-popup/magnific-popup.css" />
+
+<!-- revolution -->
+<link rel="stylesheet" type="text/css" href="revolution/css/settings.css" />
+
+<!-- main style -->
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+<!-- responsive -->
+<link rel="stylesheet" type="text/css" href="css/responsive.css" />
+</head>
+<body>
+
+</body>
+</html>
 <h1>this header 1</h1>
+@include('front-end.components.feature-car')
