3ff24f5618af1c3d3681dece4a2b7003eb1204d9
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-05-21 18:00:22 +0430

    Refactor code structure for improved readability and maintainability

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,38 +7,351 @@
     <title>Document</title>
 
     <!-- Favicon -->
-<link rel="shortcut icon" href="images/favicon.ico" />
+<link rel="stylesheet" href="{{asset('images/favicon.ico')}}"/>
 
 <!-- bootstrap -->
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+<link rel="stylesheet" href="{{asset('css/')}}"/>
 
 <!-- flaticon -->
-<link rel="stylesheet" type="text/css" href="css/flaticon.css" />
+<link rel="stylesheet" href="{{asset('css/flaticon.css')}}"/>
 
 <!-- mega menu -->
-<link rel="stylesheet" type="text/css" href="css/mega-menu/mega_menu.css" />
+<link rel="stylesheet" href="{{asset('css/mega-menu/responsive.css')}}"/>
 
 <!-- font awesome -->
-<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+<link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}"/>
 
 <!-- owl-carousel -->
-<link rel="stylesheet" type="text/css" href="css/owl-carousel/owl.carousel.css" />
+<link rel="stylesheet" href="{{asset('css/owl-carousel/owl.theme.css')}}"/>
 
 <!-- magnific-popup -->
-<link rel="stylesheet" type="text/css" href="css/magnific-popup/magnific-popup.css" />
+<link rel="stylesheet" href="{{asset('css/magnific-popup/magnific-popup.css')}}"/>
+
 
 <!-- revolution -->
-<link rel="stylesheet" type="text/css" href="revolution/css/settings.css" />
+<link rel="stylesheet" href="{{asset('revolution/css/sittings.css')}}"/>
 
 <!-- main style -->
-<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" href="{{asset('css/style.css')}}"/>
 
 <!-- responsive -->
-<link rel="stylesheet" type="text/css" href="css/responsive.css" />
+<link rel="stylesheet" href="{{asset('css/responsive.css')}}"/>
+
+
 </head>
 <body>
+    <h1>this header 1</h1>
+   {{-- feature cards --}}
+   <section class="feature-car bg-2 bg-overlay-black-70 page-section-ptb">
+  <div class="container">
+   <div class="row">
+    <div class="col-md-12">
+      <div class="section-title">
+         <span class="text-white">Check out our recent cars</span>
+         <h2 class="text-white">Feature Car </h2>
+         <div class="separator"></div>
+      </div>
+    </div>
+   </div>
+   <div class="row">
+    <div class="col-md-12">
+     <div class="owl-carousel owl-theme" data-nav-arrow="true" data-items="4" data-md-items="4" data-sm-items="2" data-xs-items="1" data-space="20">
+      <div class="item">
+       <div class="car-item text-center">
+         <div class="car-image">
+           <img class="img-fluid" src="{{asset('images/car/01.jpg')}}" alt="">
+           <div class="car-overlay-banner">
+            <ul>
+              <li><a href="#"><i class="fa fa-link"></i></a></li>
+              <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+             </ul>
+           </div>
+         </div>
+         <div class="car-list">
+           <ul class="list-inline">
+             <li><i class="fa fa-registered"></i> 2021</li>
+             <li><i class="fa fa-cog"></i> Manual </li>
+             <li><i class="fa fa-dashboard"></i> 6,000 mi</li>
+           </ul>
+        </div>
+         <div class="car-content">
+          <div class="star">
+           <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star-o orange-color"></i>
+           </div>
+           <a href="#">Acura Rsx</a>
+           <div class="separator"></div>
+           <div class="price">
+             <span class="old-price">$35,568</span>
+             <span class="new-price">$32,698 </span>
+           </div>
+         </div>
+       </div>
+     </div>
+     <div class="item">
+       <div class="car-item text-center">
+         <div class="car-image">
+           <img class="img-fluid" src="images/car/02.jpg" alt="">
+           <div class="car-overlay-banner">
+            <ul>
+              <li><a href="#"><i class="fa fa-link"></i></a></li>
+              <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+             </ul>
+           </div>
+         </div>
+         <div class="car-list">
+           <ul class="list-inline">
+             <li><i class="fa fa-registered"></i> 2021</li>
+             <li><i class="fa fa-cog"></i> Manual </li>
+             <li><i class="fa fa-dashboard"></i> 6,000 mi</li>
+           </ul>
+        </div>
+         <div class="car-content">
+          <div class="star">
+           <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star-o orange-color"></i>
+           </div>
+           <a href="#">Lexus GS 450h</a>
+           <div class="separator"></div>
+           <div class="price">
+             <span class="old-price">$35,568</span>
+             <span class="new-price">$32,698 </span>
+           </div>
+         </div>
+       </div>
+     </div>
+     <div class="item">
+       <div class="car-item text-center">
+         <div class="car-image">
+           <img class="img-fluid" src="images/car/03.jpg" alt="">
+           <div class="car-overlay-banner">
+            <ul>
+              <li><a href="#"><i class="fa fa-link"></i></a></li>
+              <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+             </ul>
+           </div>
+         </div>
+         <div class="car-list">
+           <ul class="list-inline">
+             <li><i class="fa fa-registered"></i> 2021</li>
+             <li><i class="fa fa-cog"></i> Manual </li>
+             <li><i class="fa fa-dashboard"></i> 6,000 mi</li>
+           </ul>
+        </div>
+         <div class="car-content">
+          <div class="star">
+           <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star-o orange-color"></i>
+           </div>
+           <a href="#">GTA 5 Lowriders DLC</a>
+           <div class="separator"></div>
+           <div class="price">
+             <span class="old-price">$35,568</span>
+             <span class="new-price">$32,698 </span>
+           </div>
+         </div>
+       </div>
+     </div>
+     <div class="item">
+       <div class="car-item text-center">
+         <div class="car-image">
+           <img class="img-fluid" src="images/car/04.jpg" alt="">
+           <div class="car-overlay-banner">
+            <ul>
+              <li><a href="#"><i class="fa fa-link"></i></a></li>
+              <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+             </ul>
+           </div>
+         </div>
+         <div class="car-list">
+           <ul class="list-inline">
+             <li><i class="fa fa-registered"></i> 2021</li>
+             <li><i class="fa fa-cog"></i> Manual </li>
+             <li><i class="fa fa-dashboard"></i> 6,000 mi</li>
+           </ul>
+        </div>
+         <div class="car-content">
+          <div class="star">
+           <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star-o orange-color"></i>
+           </div>
+           <a href="#">Toyota avalon hybrid </a>
+           <div class="separator"></div>
+           <div class="price">
+             <span class="old-price">$35,568</span>
+             <span class="new-price">$32,698 </span>
+           </div>
+         </div>
+       </div>
+      </div>
+      <div class="item">
+       <div class="car-item text-center">
+         <div class="car-image">
+           <img class="img-fluid" src="images/car/05.jpg" alt="">
+           <div class="car-overlay-banner">
+            <ul>
+              <li><a href="#"><i class="fa fa-link"></i></a></li>
+              <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+             </ul>
+           </div>
+         </div>
+         <div class="car-list">
+           <ul class="list-inline">
+             <li><i class="fa fa-registered"></i> 2021</li>
+             <li><i class="fa fa-cog"></i> Manual </li>
+             <li><i class="fa fa-dashboard"></i> 6,000 mi</li>
+           </ul>
+        </div>
+         <div class="car-content">
+          <div class="star">
+           <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star orange-color"></i>
+            <i class="fa fa-star-o orange-color"></i>
+           </div>
+           <a href="#">Hyundai santa fe sport </a>
+           <div class="separator"></div>
+           <div class="price">
+             <span class="old-price">$35,568</span>
+             <span class="new-price">$32,698 </span>
+           </div>
+         </div>
+       </div>
+      </div>
+     </div>
+    </div>
+   </div>
+  </div>
+</section>
+
+<!-- Feature Car End -->
+
+
+
+
+
+    <!--=================================
+ jquery -->
+
+<!-- jquery  -->
+<script type="text/javascript" src="{{asset('js/js/jquery-3.6.0.min.js')}}"></script>
+
+<!-- bootstrap -->
+<script type="text/javascript" src="{{asset('js/popper.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+
+<!-- mega-menu -->
+<script type="text/javascript" src="{{asset('js/mega-menu/mega_menu.js')}}"></script>
+
+<!-- appear -->
+<script type="text/javascript" src="{{asset('js/jquery.appear.js')}}"></script>
+
+<!-- counter -->
+<script type="text/javascript" src="{{asset('js/counter/jquery.countTo.js')}}"></script>
+
+<!-- owl-carousel -->
+<script type="text/javascript" src="{{asset('js/owl-carousel/owl.carousel.min.js')}}"></script>
+
+<!-- select -->
+<script type="text/javascript" src="{{asset('js/select/jquery-select.js')}}"></script>
+
+<!-- magnific popup -->
+<script type="text/javascript" src="{{asset('js/magnific-popup/jquery.magnific-popup.min.js')}}"></script>
+
+<!-- revolution -->
+<script type="text/javascript" src="{{asset('revolution/js/jquery.themepunch.tools.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('revolution/js/jquery.themepunch.revolution.min.js')}}"></script>
+<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+<script type="text/javascript" src="{{asset('revolution/js/extensions/revolution.extension.actions.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('revolution/js/extensions/revolution.extension.carousel.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('revolution/js/extensions/revolution.extension.kenburn.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('revolution/js/extensions/revolution.extension.layeranimation.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('revolution/js/extensions/revolution.extension.migration.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('revolution/js/extensions/revolution.extension.navigation.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('revolution/js/extensions/revolution.extension.parallax.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('revolution/js/extensions/revolution.extension.slideanims.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('revolution/js/extensions/revolution.extension.video.min.js')}}"></script>
+
+<!-- custom -->
+<script type="text/javascript" src="{{asset('js/custom.js')}}"></script>
+
+<script type="text/javascript">
+   (function($){
+  "use strict";
+
+    var tpj=jQuery;
+      var revapi2;
+      tpj(document).ready(function() {
+        if(tpj("#rev_slider_2_1").revolution == undefined){
+          revslider_showDoubleJqueryError("#rev_slider_2_1");
+        }else{
+          revapi2 = tpj("#rev_slider_2_1").show().revolution({
+            sliderType:"standard",
+            sliderLayout:"fullwidth",
+            dottedOverlay:"none",
+            delay:9000,
+            navigation: {
+              keyboardNavigation:"off",
+              keyboard_direction: "horizontal",
+              mouseScrollNavigation:"off",
+                             mouseScrollReverse:"default",
+              onHoverStop:"off",
+              bullets: {
+                enable:true,
+                hide_onmobile:false,
+                style:"hermes",
+                hide_onleave:false,
+                direction:"horizontal",
+                h_align:"center",
+                v_align:"bottom",
+                h_offset:0,
+                v_offset:50,
+                                space:10,
+                tmp:''
+              }
+            },
+            visibilityLevels:[1240,1024,778,480],
+            gridwidth:1570,
+            gridheight:1000,
+            lazyType:"none",
+            shadow:0,
+            spinner:"spinner3",
+            stopLoop:"off",
+            stopAfterLoops:-1,
+            stopAtSlide:-1,
+            shuffle:"off",
+            autoHeight:"off",
+            disableProgressBar:"on",
+            hideThumbsOnMobile:"off",
+            hideSliderAtLimit:0,
+            hideCaptionAtLimit:0,
+            hideAllCaptionAtLilmit:0,
+            debugMode:false,
+            fallbacks: {
+              simplifyAll:"off",
+              nextSlideOnWindowFocus:"off",
+              disableFocusListener:false,
+            }
+          });
+        }
+      });
+  })(jQuery);
+
+</script>
+
 
 </body>
 </html>
-<h1>this header 1</h1>
-@include('front-end.components.feature-car')
