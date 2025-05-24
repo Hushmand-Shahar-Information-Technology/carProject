d97e86485b3479792b5f0039a11828237ce7fb67
ozair0101 <ozairjan186@gmail.com>
2025-05-24 17:06:50 +0430

    create seeder for car table and also create list of car page

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Car;
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    
+    /**
+     * Display the car index.
+     */
+    public function index()
+    {
+        $cars = Car::all();
+        return view('car.index', compact('cars'));
+    }
+
+}
--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -2,16 +2,47 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use App\Models\Car;
 
 class CarSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $imageFiles = File::files(public_path('images/car'));
+        $imagePaths = array_map(function ($file) {
+            return 'images/car/' . $file->getFilename();
+        }, $imageFiles);
+
+        $makes = ['Toyota', 'Ford', 'Chevrolet', 'Honda', 'BMW', 'Audi', 'Nissan', 'Ferrari', 'Bugati'];
+        $models = ['Corolla', 'Civic', 'Accord', 'Camry', 'Mustang', 'Altima', 'X5', '4Ruuner'];
+
+        for ($i = 0; $i < 20; $i++) {
+            $make = $makes[array_rand($makes)];
+            $model = $models[array_rand($models)];
+            $year = rand(2000, 2024);
+            $regularPrice = rand(5000, 30000);
+            $salePrice = rand(4000, $regularPrice);
+
+            $numImages = rand(1, 5);
+            $images = array_rand($imagePaths, min($numImages, count($imagePaths)));
+            $images = is_array($images) ? array_map(fn($i) => $imagePaths[$i], $images) : [$imagePaths[$images]];
+
+            Car::create([
+                'title' => "$year $make $model",
+                'year' => $year,
+                'make' => $make,
+                'location' => json_encode(['city' => 'Sample City', 'state' => 'CA']),
+                'model' => $model,
+                'VIN_number' => strtoupper(Str::random(17)),
+                'regular_price' => $regularPrice,
+                'sale_price' => $salePrice,
+                'request_price_status' => (bool)rand(0, 1),
+                'request_price' => rand(4000, 50000),
+                'images' => json_encode($images),
+            ]);
+        }
     }
 }
