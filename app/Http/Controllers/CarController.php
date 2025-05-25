2b66c7e5d37ccfecc74b9cd169721cd47f12bb25
ozair0101 <ozairjan186@gmail.com>
2025-05-25 16:22:49 +0430

    edit car list add some filter to it

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -14,7 +14,34 @@ class CarController extends Controller
     public function index()
     {
         $cars = Car::all();
-        return view('car.index', compact('cars'));
+        return view('car.car-listing', compact('cars'));
     }
+
+    /**
+     * Filter the cars.
+     */
+   public function filter(Request $request)
+    {
+        $cars = Car::query();
+
+        if ($request->filled('year')) {
+            $cars->whereIn('year', $request->year);
+        }
+        if ($request->filled('condition')) {
+            $cars->whereIn('condition', $request->condition);
+        }
+        if ($request->filled('model')) {
+            $cars->whereIn('model', $request->model);
+        }
+        // ... Add more filters as needed ...
+
+        $filteredCars = $cars->latest()->get();
+
+        if ($request->ajax()) {
+            return view('car.car-results', compact('filteredCars'))->render();
+        }
+        
+    }
+
 
 }
--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\enum\TransmissionType;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
@@ -9,40 +10,58 @@ use App\Models\Car;
 
 class CarSeeder extends Seeder
 {
-    public function run(): void
-    {
-        $imageFiles = File::files(public_path('images/car'));
-        $imagePaths = array_map(function ($file) {
-            return 'images/car/' . $file->getFilename();
-        }, $imageFiles);
+   
+public function run(): void
+{
+    $imageFiles = File::files(public_path('images/car'));
+    $imagePaths = array_map(function ($file) {
+        return 'images/car/' . $file->getFilename();
+    }, $imageFiles);
 
-        $makes = ['Toyota', 'Ford', 'Chevrolet', 'Honda', 'BMW', 'Audi', 'Nissan', 'Ferrari', 'Bugati'];
-        $models = ['Corolla', 'Civic', 'Accord', 'Camry', 'Mustang', 'Altima', 'X5', '4Ruuner'];
+    $makes = ['Toyota', 'Ford', 'Chevrolet', 'Honda', 'BMW', 'Audi', 'Nissan', 'Ferrari', 'Bugati'];
+    $models = ['Corolla', 'Civic', 'Accord', 'Camry', 'Mustang', 'Altima', 'X5', '4Runner'];
+    
+    // Add your own color palette
+    $colors = [
+        'Ruby Red Metallic',
+        'Racing Yellow',
+        'Guards Red',
+        'Aqua Blue Metallic',
+        'White',
+        'Dark Blue Metallic',
+        'Black',
+        'Silver',
+        'Platinum Grey',
+    ];
 
-        for ($i = 0; $i < 20; $i++) {
-            $make = $makes[array_rand($makes)];
-            $model = $models[array_rand($models)];
-            $year = rand(2000, 2024);
-            $regularPrice = rand(5000, 30000);
-            $salePrice = rand(4000, $regularPrice);
+    for ($i = 0; $i < 20; $i++) {
+        $make = $makes[array_rand($makes)];
+        $model = $models[array_rand($models)];
+        $year = rand(2000, 2024);
+        $regularPrice = rand(5000, 30000);
+        $salePrice = rand(4000, $regularPrice);
 
-            $numImages = rand(1, 5);
-            $images = array_rand($imagePaths, min($numImages, count($imagePaths)));
-            $images = is_array($images) ? array_map(fn($i) => $imagePaths[$i], $images) : [$imagePaths[$images]];
+        $numImages = rand(1, 5);
+        $images = array_rand($imagePaths, min($numImages, count($imagePaths)));
+        $images = is_array($images) ? array_map(fn($i) => $imagePaths[$i], $images) : [$imagePaths[$images]];
 
-            Car::create([
-                'title' => "$year $make $model",
-                'year' => $year,
-                'make' => $make,
-                'location' => json_encode(['city' => 'Sample City', 'state' => 'CA']),
-                'model' => $model,
-                'VIN_number' => strtoupper(Str::random(17)),
-                'regular_price' => $regularPrice,
-                'sale_price' => $salePrice,
-                'request_price_status' => (bool)rand(0, 1),
-                'request_price' => rand(4000, 50000),
-                'images' => json_encode($images),
-            ]);
-        }
+        Car::create([
+            'title' => "$year $make $model",
+            'year' => $year,
+            'make' => $make,
+            'location' => json_encode(['city' => 'Sample City', 'state' => 'CA']),
+            'model' => $model,
+            'VIN_number' => strtoupper(Str::random(17)),
+            'regular_price' => $regularPrice,
+            'sale_price' => $salePrice,
+            'request_price_status' => (bool)rand(0, 1),
+            'request_price' => rand(4000, 50000),
+            'images' => json_encode($images),
+
+            // New columns
+            'color' => $colors[array_rand($colors)],
+            'transmission_type' => TransmissionType::cases()[array_rand(TransmissionType::cases())]->value,
+        ]);
     }
+}
 }
