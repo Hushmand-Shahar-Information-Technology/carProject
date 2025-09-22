b4a15a301e759cb0b46f2a4f3cb5375fb6ba12f8
Ozair0101 <ozairjan@gmail.com>
2025-09-22 14:39:01 +0430

    add console

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::command('auctions:end-expired')
+    ->everyMinute()
+    ->name('end-expired-auctions')
+    ->withoutOverlapping()
+    ->runInBackground();
+
+Schedule::command('test:scheduler')
+    ->everyFiveMinutes()
+    ->name('test-scheduler')
+    ->withoutOverlapping()
+    ->runInBackground();
