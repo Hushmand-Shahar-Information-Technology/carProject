0f3a7e052426e78c9ec39d9e32506bf4b736cc49
Ozair0101 <ozairjan@gmail.com>
2025-10-02 12:48:45 +0430

    Create email controller

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Email;
+use Illuminate\Http\Request;
+
+class EmailController extends Controller
+{
+    public function store(Request $request) {
+        
+        $request->validate([
+        'email' => ['required','email']
+        ]);
+
+        Email::create($request->all());
+        
+        return response()->json(['success' => true]);
+    }
+}
